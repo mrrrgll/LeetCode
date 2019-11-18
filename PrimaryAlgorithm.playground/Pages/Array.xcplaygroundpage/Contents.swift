3208b0cf96253143f3461194a966677f3a06e718
Sven <1933455660@qq.com>
2019-11-19 01:21:02 +0800

    从排序数组中删除重复项

--- PrimaryAlgorithm.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ PrimaryAlgorithm.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -1,7 +1,22 @@
-//: [Previous](@previous)
-
 import Foundation
+//: [Previous](@previous)
+//:
+//: ---
+//: ### [从排序数组中删除重复项](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/21/)
+func removeDuplicates(_ nums: inout [Int]) -> Int {
+    if nums.isEmpty { return 0 }
+    var i = 0
+    for item in nums where item != nums[i] {
+        i += 1
+        nums[i] = item
+    }
+    return i + 1
+}
+//:
+//: ---
+//: ### [买卖股票的最佳时机 II](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/22/)
 
-var str = "Hello, playground"
+
+
 
 //: [Next](@next)
