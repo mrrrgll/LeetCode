21163795deab545edfa00e9c9e0d118e6c869d64
Sven <1933455660@qq.com>
2019-11-19 18:54:10 +0800

    买卖股票的最佳时机 II

--- PrimaryAlgorithm.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ PrimaryAlgorithm.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -15,8 +15,22 @@ func removeDuplicates(_ nums: inout [Int]) -> Int {
 //:
 //: ---
 //: ### [买卖股票的最佳时机 II](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/22/)
-
-
-
+func maxProfit(_ prices: [Int]) -> Int {
+    if prices.count < 2 { return 0 }
+    var sum = 0
+    var i = 0
+    var j = 1
+    while j < prices.count {
+        if prices[j] > prices[i] {
+            sum += prices[j] - prices[i]
+        }
+        j += 1
+        i += 1
+    }
+    return sum
+}
+//:
+//: ---
+//: ### []()
 
 //: [Next](@next)
