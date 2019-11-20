b89760a38f909916dad8b258b7c00254f05cd9be
Sven <1933455660@qq.com>
2019-11-21 01:14:54 +0800

    两个数组的交集 II

--- PrimaryAlgorithm.playground/Pages/Array.xcplaygroundpage/Contents.swift
+++ PrimaryAlgorithm.playground/Pages/Array.xcplaygroundpage/Contents.swift
@@ -65,5 +65,41 @@ func singleNumber(_ nums: [Int]) -> Int {
     }
     return res
 }
+//:
+//: ---
+//: ###[两个数组的交集 II](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/1/array/26/)
+func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+    var frequency = Dictionary(nums1.map{ ($0, 1) }, uniquingKeysWith: +)
+    var results = [Int]()
+    for num in nums2 {
+        if let value = frequency[num], value > 0 {
+            frequency[num] = value - 1
+            results.append(num)
+        }
+    }
+    return results
+    /* 先排序,再找
+    var res = [Int]()
+    let n1 = nums1.sorted()
+    let n2 = nums2.sorted()
+    var i = 0
+    var j = 0
+    while i < n1.count && j < n2.count {
+        if n1[i] == n2[j] {
+            res.append(n1[i])
+            i += 1
+            j += 1
+        } else if n1[i] < n2[j] {
+            i += 1
+        } else {
+            j += 1
+        }
+    }
+    return res
+     */
+}
+
+let nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+intersect(nums1, nums2)
 
 //: [Next](@next)
