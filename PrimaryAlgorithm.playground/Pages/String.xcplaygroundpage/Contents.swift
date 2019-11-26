a65f33be2894e327dd1452818c3bebe9adee1cee
Sven <1933455660@qq.com>
2019-11-26 21:36:00 +0800

    反转字符串

--- PrimaryAlgorithm.playground/Pages/String.xcplaygroundpage/Contents.swift
+++ PrimaryAlgorithm.playground/Pages/String.xcplaygroundpage/Contents.swift
@@ -1,7 +1,17 @@
-//: [Previous](@previous)
-
 import Foundation
-
-var str = "Hello, playground"
+//: [Previous](@previous)
+//:
+//: ---
+//: ### [反转字符串](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/32/)
+func reverseString(_ s: inout [Character]) {
+    let c = s.count
+    let mid = c / 2
+    for i in 0..<mid {
+        (s[i], s[c - 1 - i]) = (s[c - 1 - i], s[i])
+    }
+    /* 直接调用API更好😂
+     s = s.reverse()
+     */
+}
 
 //: [Next](@next)
