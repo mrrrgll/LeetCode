5771f2404b3293e53848340aba960c602246763d
Sven <1933455660@qq.com>
2019-12-03 00:35:34 +0800

    反转链表

--- PrimaryAlgorithm.playground/Pages/List.xcplaygroundpage/Contents.swift
+++ PrimaryAlgorithm.playground/Pages/List.xcplaygroundpage/Contents.swift
@@ -50,10 +50,45 @@ func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
     prev!.next = prev!.next!.next
     return dummy.next
 }
+//:
+//: ---
+//: ### [反转链表](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/6/linked-list/43/)
+func reverseList(_ head: ListNode?) -> ListNode? {
+    // 迭代解法
+    var pre: ListNode?
+    var cur = head
+    while cur != nil {
+        let tmp = cur?.next
+        cur?.next = pre
+        pre = cur
+        cur = tmp
+    }
+    return pre
+    /* 递归解法
+     if head == nil || head?.next == nil {
+         return head
+     }
+     let cur = reverseList(head!.next)
+     head!.next?.next = head
+     head!.next = nil
+     return cur
+     */
+}
+
+let t1 = ListNode(1)
+let t2 = ListNode(2)
+let t3 = ListNode(3)
+let t4 = ListNode(4)
+let t5 = ListNode(5)
+
+t1.next = t2
+t2.next = t3
+t3.next = t4
+t4.next = t5
 
 let startTime = CFAbsoluteTimeGetCurrent()
-
+reverseList(t1)
 let endTime = CFAbsoluteTimeGetCurrent()
-print("代码执行时长：\((endTime - startTime)*1000) 毫秒")
+print((endTime - startTime)*1000)
 
 //: [Next](@next)
