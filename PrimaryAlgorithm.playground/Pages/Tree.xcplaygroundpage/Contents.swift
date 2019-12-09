2a1df00777a4f3111b4307d5efcb23139a62efbf
Sven <1933455660@qq.com>
2019-12-09 20:20:46 +0800

    no message

--- PrimaryAlgorithm.playground/Pages/Tree.xcplaygroundpage/Contents.swift
+++ PrimaryAlgorithm.playground/Pages/Tree.xcplaygroundpage/Contents.swift
@@ -15,33 +15,38 @@ class TreeNode {
 //: ### [二叉树的最大深度](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/47/)
 func maxDepth(_ root: TreeNode?) -> Int {
     
+    return 0
 }
 //:
 //: ---
 //: ### [验证二叉搜索树](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/48/)
 func isValidBST(_ root: TreeNode?) -> Bool {
     
+    return false
 }
 //:
 //: ---
 //: ### [对称二叉树](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/49/)
 func isSymmetric(_ root: TreeNode?) -> Bool {
     
+    return false
 }
 //:
 //: ---
 //: ### [二叉树的层次遍历](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/50/)
 func levelOrder(_ root: TreeNode?) -> [[Int]] {
     
+    return []
 }
 //:
 //: ---
 //: ### [将有序数组转换为二叉搜索树](https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/7/trees/51/)
 func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
     
+    return nil
 }
 let startTime = CFAbsoluteTimeGetCurrent()
 
 let endTime = CFAbsoluteTimeGetCurrent()
-print((endTime - startTime)1000)
+print((endTime - startTime)*1000)
 //: [Next](@next)
